37294b0c246c78f57b52be8a6acdc7a164fb368d
zolaFotsiny <zolaandriamarosoa@gmail.com>
2024-02-08 01:16:44 +0100

    bug fix problem cors

--- src/app.js
+++ src/app.js
@@ -5,15 +5,16 @@ const bodyParser = require('body-parser');
 const clientRoutes = require('./routes/users.route');
 const authRoutes = require('./routes/auth.route');
 const serviceRoutes = require('./routes/service.routes');
-
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors()); // Enable CORS before any routes
 app.use(bodyParser.json());
 app.use('/users', clientRoutes);
 app.use('/auth', authRoutes);
 app.use('/services', serviceRoutes);
-
+app.use('/uploads', express.static('public/uploads'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
