3d7901121356f7d38364478683c035b799214c84
zolaFotsiny <zolaandriamarosoa@gmail.com>
2024-02-22 23:26:06 +0100

    Update tache.routes.js

--- src/routes/tache.routes.js
+++ src/routes/tache.routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const tacheController = require('../controllers/tache.controller');
 
 router.get('/', tacheController.findAll);
-router.get('/getDailyTasks', tacheController.getDailyTasksAndCommission);
 router.put('/validateTask/:id', tacheController.validateTask); // Ajout de la nouvelle route
 
 module.exports = router;
