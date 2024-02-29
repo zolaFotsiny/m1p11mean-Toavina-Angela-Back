c2f22250e5d3b90d1372a068807d858d68413659
zolaFotsiny <zolaandriamarosoa@gmail.com>
2024-02-29 10:48:42 +0100

    paiement comission

--- src/controllers/comission.controller.js
+++ src/controllers/comission.controller.js
@@ -1,7 +1,8 @@
 const Commission = require('../models/comission.model'); // Assuming the schema is in models/comission.model.js
 const tokenUtils = require('../utils/token');
 const utilDB = require('../utils/utilDB');
-
+const Depense = require('../models/depense.model');
+const DepenseDetails = require('../models/depensedetails.model');
 async function findAll(req, res) {
     try {
         await utilDB.connect();
@@ -41,7 +42,52 @@ async function findAll(req, res) {
         utilDB.close();
     }
 }
+async function creerDepense(req, res) {
+    try {
+        await utilDB.connect();
+
+        // Retrieve the commission id from the request
+        const idCommission = req.params.id;
+        console.log("test", idCommission);
+
+        // Find the commission with the given id
+        const commission = await Commission.findById(idCommission).populate('id_service');
+        console.log("commission", commission);
+        if (!commission) {
+            return res.status(404).json({ message: 'Commission not found' });
+        }
+
+        // Create a new expense
+        const depense = await Depense.create({
+            montant_total: commission.montant,
+            date_depense: new Date(),
+            mode_depense: 'Cash', // Set the payment method to 'Cash'
+            etat: 1,
+        });
+
+        // Create the expense details
+        const depenseDetails = await DepenseDetails.create({
+            id_depense: depense._id,
+            id_tache: commission.id_tache,
+            id_service: commission.id_service,
+            designation: 'Commission', // Replace with the actual designation
+            montant: commission.montant,
+            etat: 1,
+        });
+
+        depense.details.push(depenseDetails);
+
+        await depense.save();
+
+        utilDB.close();
+        res.status(201).json({ message: 'Expense created successfully', depense: depense });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error during expense creation' });
+    }
+}
 
 module.exports = {
-    findAll
+    findAll,
+    creerDepense
 };
--- src/routes/comission.routes.js
+++ src/routes/comission.routes.js
@@ -5,7 +5,7 @@ const comission = require('../controllers/comission.controller');
 const router = express.Router();
 
 router.get('/', comission.findAll);
-
+router.put('/valider/:id', comission.creerDepense);
 
 
 module.exports = router;
